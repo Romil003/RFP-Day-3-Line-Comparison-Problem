77a6aa6d409b26b2e549b028c20f7a6ca51d25ae
Romil003 <romilghadge33@gmail.com>
2022-09-29 18:53:02 +0530

    Added Line Comparison UC2

--- src/com/bridgelabz/LineComparison.java
+++ src/com/bridgelabz/LineComparison.java
@@ -4,30 +4,41 @@ import java.util.Scanner;
 
 public class LineComparison {
 
-    static void calculateLength(int x1,int y1,int x2,int y2){
-//        int a = x2-x1 ;
-//        int res1 = a*a;
-//        int b = y2-y1;
-//        int res2 = b*b;
-//        double length = Math.sqrt(res1+res2);
-//        System.out.println(length);
-        double Length = Math.sqrt(Math.pow(x2-x1,2)+Math.pow(y2-y1,2));
-        System.out.println(Length);
+    static void calculateLength(int x11,int y11,int x12,int y12,int x21,int y21,int x22,int y22){
+
+        double line1Length = Math.sqrt(Math.pow(x12-x11,2)+Math.pow(y12-y11,2));
+        System.out.println(line1Length);
+        double line2Length = Math.sqrt(Math.pow(x22-x21,2)+Math.pow(y22-y21,2));
+        System.out.println(line2Length);
+        if (line1Length == line2Length){
+            System.out.println("Line 1 length is equal to Line 2 length");
+        }
+        else {
+            System.out.println("Line 1 length is not equal to Line 2 length");
+        }
 
 
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter x1 co-ordinate");
-        int x1 = sc.nextInt();
-        System.out.println("Enter y1 co-ordinate");
-        int y1 = sc.nextInt();
-        System.out.println("Enter x2 co-ordinate");
-        int x2 = sc.nextInt();
-        System.out.println("Enter y2 co-ordinate");
-        int y2 = sc.nextInt();
-        calculateLength(x1,y1,x2,y2);
+        System.out.println("Enter Line1 x11 co-ordinate");
+        int x11 = sc.nextInt();
+        System.out.println("Enter Line1 y11 co-ordinate");
+        int y11 = sc.nextInt();
+        System.out.println("Enter Line1 x12 co-ordinate");
+        int x12 = sc.nextInt();
+        System.out.println("Enter Line1 y12 co-ordinate");
+        int y12 = sc.nextInt();
+        System.out.println("Enter Line2 x21 co-ordinate");
+        int x21 = sc.nextInt();
+        System.out.println("Enter Line2 y21 co-ordinate");
+        int y21 = sc.nextInt();
+        System.out.println("Enter Line2 x22 co-ordinate");
+        int x22 = sc.nextInt();
+        System.out.println("Enter Line2 y22 co-ordinate");
+        int y22 = sc.nextInt();
+        calculateLength(x11,y11,x12,y12,x21,y21,x22,y22);
 
     }
 }
