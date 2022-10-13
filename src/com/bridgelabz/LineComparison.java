9dde135282eac9510281722a171c562ecd275b87
Romil003 <romilghadge33@gmail.com>
2022-10-13 18:27:58 +0530

    Comparing two line using OOPs concepts

--- src/com/bridgelabz/LineComparison.java
+++ src/com/bridgelabz/LineComparison.java
@@ -4,16 +4,27 @@ import java.util.Scanner;
 
 public class LineComparison {
 
-    static void calculateLength(double x11,double y11,double x12,double y12,double x21,double y21,double x22,double y22){
+    static double calculateLine1Length(double x11,double y11,double x12,double y12){
 
-        Double line1Length = Math.sqrt(Math.pow(x12-x11,2)+Math.pow(y12-y11,2));
-        System.out.println("Line 1 length = " + line1Length);
-        System.out.println();
-        Double line2Length = Math.sqrt(Math.pow(x22-x21,2)+Math.pow(y22-y21,2));
-        System.out.println("Line 2 length = " + line2Length);
-        System.out.println();
+        double line1Length = Math.sqrt(Math.pow(x12-x11,2)+Math.pow(y12-y11,2));
 
-        int res = line1Length.compareTo(line2Length);
+        return line1Length ;
+
+
+    }
+
+    static double calculateLine2Length(double x21,double y21,double x22,double y22){
+        double line2Length = Math.sqrt(Math.pow(x22-x21,2)+Math.pow(y22-y21,2));
+
+        return line2Length;
+    }
+
+    static void comparingLengthOfLines(double length1,double length2 ){
+
+        Double line1 = Double.valueOf(length1);
+        Double line2 = Double.valueOf(length2);
+
+        int res = line1.compareTo(line2);
 
         if (res == 0){
             System.out.println("Line 1 length is equal to Line 2 length");
@@ -24,7 +35,21 @@ public class LineComparison {
         else {
             System.out.println("Line 1 length is smaller than Line 2 length");
         }
+    }
 
+    static void checkingEqualityOfLines(double length1 , double length2 ){
+
+        Double line1 = Double.valueOf(length1);
+        Double line2 = Double.valueOf(length2);
+
+        boolean result = line1.equals(line2);
+
+        if (result == true){
+            System.out.println("Line 1 length is equal to Line 2 length");
+        }
+        else {
+            System.out.println("Line 1 length is not equal to Line 2 length");
+        }
 
     }
 
@@ -47,7 +72,16 @@ public class LineComparison {
         System.out.println("Enter Line2 y22 co-ordinate");
         double y22 = sc.nextInt();
         System.out.println();
-        calculateLength(x11,y11,x12,y12,x21,y21,x22,y22);
+        double line1 = calculateLine1Length(x11,y11,x12,y12);
+        System.out.println("Line 1 length = " + line1);
+        double line2 = calculateLine2Length(x21,y21,x22,y22);
+        System.out.println("Line 1 length = " + line2);
+        System.out.println();
+        checkingEqualityOfLines(line1,line2);
+        System.out.println();
+        comparingLengthOfLines(line1,line2);
+
+
 
     }
 }
